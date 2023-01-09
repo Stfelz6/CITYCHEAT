047a13a5055b6ece6480477ed07adb479520eb3d
Stfelz6 <101118482+Stfelz6@users.noreply.github.com>
2023-01-09 12:07:34 +0200

    final

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
-$(function(){
+// // $(function(){
 
-    $(".dropdown-menu").on('click', 'li a', function(){
-      $(".btn:first-child").text($(this).text());
-      $(".btn:first-child").val($(this).text());
-   });
+// //     $(".dropdown-menu").on('click', 'li a', function(){
+// //       $(".btn:first-child").text($(this).text());
+// //       $(".btn:first-child").val($(this).text());
+// //    });
 
-});
+// // });
+
+window.onkeydown = function(evt) {
+  if (evt.keyCode == "49") {
+      alert("You chose the tram");
+  }
+  else if (evt.keyCode == "50") {
+      alert("You chose the subway");
+  }
+  else if (evt.keyCode == "51") {
+      alert("You chose the bus");
+  }
+  else if (evt.keyCode == "52") {
+      alert("You chose the taxi");
+  }
+  else{
+      alert("It's not a valid option");
+  }
+};
