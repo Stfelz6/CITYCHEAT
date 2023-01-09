bc1d58b3ed55c92cc58bf3d0e1f92528ce70a9a7
Stfelz6 <101118482+Stfelz6@users.noreply.github.com>
2023-01-09 12:56:40 +0200

    the end

--- index.js
+++ index.js
@@ -1,26 +1,40 @@
-// // $(function(){
-
-// //     $(".dropdown-menu").on('click', 'li a', function(){
-// //       $(".btn:first-child").text($(this).text());
-// //       $(".btn:first-child").val($(this).text());
-// //    });
-
-// // });
-
 window.onkeydown = function(evt) {
-  if (evt.keyCode == "49") {
-      alert("You chose the tram");
-  }
-  else if (evt.keyCode == "50") {
-      alert("You chose the subway");
-  }
-  else if (evt.keyCode == "51") {
-      alert("You chose the bus");
-  }
-  else if (evt.keyCode == "52") {
-      alert("You chose the taxi");
-  }
-  else{
-      alert("It's not a valid option");
-  }
+   var trainNo = document.getElementById('trainNo');
+   var subwayNo = document.getElementById('subwayNo');
+   var busNo = document.getElementById('busNo');
+   var taxiNo = document.getElementById('taxiNo');
+
+ if (evt.keyCode == "49") {
+     //alert("You chose the tram");
+     trainNo.style.display='block';
+     subwayNo.style.display='none';
+     busNo.style.display='none';
+     taxiNo.style.display='none';
+ }
+ else if (evt.keyCode == "50") {
+     //alert("You chose the subway");
+     trainNo.style.display='none';
+     subwayNo.style.display='block';
+     busNo.style.display='none';
+     taxiNo.style.display='none';
+ }
+ else if (evt.keyCode == "51") {
+     //alert("You chose the bus");
+     trainNo.style.display='none';
+     subwayNo.style.display='none';
+     busNo.style.display='block';
+     taxiNo.style.display='none';
+ }
+ else if (evt.keyCode == "52") {
+     //alert("You chose the taxi");
+     trainNo.style.display='none';
+     subwayNo.style.display='none';
+     busNo.style.display='none';
+     taxiNo.style.display='block';
+ }
 };
+
+let myVar = setInterval(myTimer, 2500);
+function myTimer() {
+ document.getElementById("logomare").style.display = 'none';
+}
